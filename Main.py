51279b1227296421b9b1ed070bbcf65a4449e317
CoombeStein <coomber@stu.otc.school.nz>
2024-06-11 10:30:36 +1200

    Turned main code into a class so my program can work :)

--- Main.py
+++ Main.py
@@ -2,9 +2,8 @@ import customtkinter as ctk
 
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("green")
-root = ctk.Ctk()
 
-
+#dictionary of all the cards values in a deck
 cards_values = {
     "A":11,
     "K":10,
@@ -21,8 +20,38 @@ cards_values = {
     "2":2,
         }
 
-player_cards=input("what cards does the player have \n(remember to leve a space between cards)\n:")
-dealer_card=input("what card is the dealer showing :")
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.total_player_score = 0
+        self.total_dealer_score = 0 
+        
+        root = ctk.CTk()
+        root.geometry('400x240')
 
+        # asking user to input player cards
+        player_card_label = ctk.CTkLabel(root, text="What cards do you have?\n remember"
+            "to awnser with only the\n number or first letter of the card")
+        player_card_label.pack()
+        player_cards = ctk.CTkEntry(root, placeholder_text="eg: A 7")
+        player_cards.bind('<Return>', self.add_score)
+        player_cards.pack()
 
-root.mainloop()
+        #asking user to input dealer cards
+        dealer_card_label= ctk.CTkLabel(root,text="What card does yhe dealer have?\n"
+            " remember to awnser with only the\n number or first letter of the card")
+        dealer_card_label.pack()
+        dealer_cards = ctk.CTkEntry(root, placeholder_text="eg: A 7")
+        dealer_cards.pack()
+
+        root.mainloop()
+    
+    def add_score(self, event):
+        player_score = event.widget.get().split()
+        for word in player_score:
+            if word.upper() in cards_values:
+                self.total_player_score = self.total_player_score + cards_values[word.upper()]
+                print(self.total_player_score)
+
+if __name__ == "__main__":
+    App()
